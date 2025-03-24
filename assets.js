0b5860bd42b1b2a595b9b21cd71179d9adaeab4b
sabonawebsite-com <sabonamarara53@gmail.com>
2025-03-24 09:47:36 +0300

    Merge remote changes

---
+++ assets.js
@@ -0,0 +1,28 @@
+import email from './email1.png'
+import github from './github.png'
+import linkedin from './linkedin.png'
+import laction from './location1.jpg'
+import facebook from './facebook1.png'
+import seach from './search1.jpg'
+import home from './home1.jpg'
+import reddit from './reddit.png'
+import tiktok from './tiktok1.png'
+import phone from './phone1.png'
+import twitter from './twitter.png'
+import pylogo from './pylogo.png'
+import youtube from './youtube1.png'
+export const assets = {
+    email,
+    github,
+    linkedin,
+    laction,
+    facebook,
+    seach,
+    home,
+    reddit,
+    tiktok,
+    phone,
+    twitter,
+    pylogo,
+    youtube
+}
